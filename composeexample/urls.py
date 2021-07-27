be2d6993978b516e93cddbd909841d2bcc74b338
Prajna <bprajna@sigmoidanalytics.com>
2021-07-27 17:52:11 +0530

    recent changes

--- composeexample/urls.py
+++ composeexample/urls.py
@@ -17,10 +17,13 @@ from django import urls
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url
+from django_prometheus import exports
 from django.conf.urls import include
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     url('', include('django_prometheus.urls')),
+    path("metrics", exports.ExportToDjangoView, name="prometheus-django-metrics")
 ]
